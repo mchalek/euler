821c753b87a5718e178feb05118eb4b46ee6c525
Kevin McHale <mchalek@gmail.com>
2013-12-10 19:50:53 -0500

    faster but still not fast enough

--- p60/p60.scala
+++ p60/p60.scala
@@ -1,15 +1,8 @@
 import collection.mutable
-import collection.immutable.ListSet
+import collection.mutable.LinkedHashSet
 import annotation.tailrec
 
-@tailrec def toListSet[A](seq: Seq[A], accum: ListSet[A] = ListSet.empty[A]): ListSet[A] = {
-  if(seq.isEmpty)
-    accum
-  else
-    toListSet(seq.init, accum + seq.last)
-}
-
-def primes(N: Int): ListSet[Int] = {
+def primes(N: Int): LinkedHashSet[Int] = {
   val composites = Array.fill(N+1)(false)
 
   val result: Seq[Int] = 
@@ -17,52 +10,63 @@ def primes(N: Int): ListSet[Int] = {
       i <- Range(2, N)
       if !composites(i)
     } yield {
-      Range(2*i, N, i).foreach(composites(_) = true)
+      for(a <- Range(2*i, N, i)) { composites(a) = true }
       i
     }
 
-  toListSet(result)
+  LinkedHashSet(result:_*)
 }
 
-def concatenations(p: Int)(implicit primes: ListSet[Int]): Seq[(Int, Int)] = {
-  def toInt(digits: String) = digits.mkString("").toInt
+def concat(a: Int, b: Int) = {
+  var i = 1
+  while(b / i != 0) {
+    i *= 10
+  }
 
-  def getPairs(digits: String) = 
-    for {
-      i <- 1 until digits.length
-      (a,b) = digits.splitAt(i) if primes((a + b).toInt)
-      pp = toInt(a) if primes(pp)
-      qq = toInt(b) if primes(qq)
-    } yield (pp, qq)
+  a*i + b
+}
 
-  val leftToRight = getPairs(p.toString)
-  val rightToLeft = getPairs(p.toString.reverse)
+def concatenations(p: Int)(implicit primes: LinkedHashSet[Int]): Seq[(Int, Int)] = {
+  @tailrec def getPairs(x: Int, div: Int = 10, accum: List[(Int, Int)] = List.empty): List[(Int, Int)] = {
+    val a = x / div
+    val b = x % div
+
+    if(a == 0)
+      return accum
+
+    if(isPrime(a) && isPrime(b)) {
+      getPairs(x, div*10, (a, b) +: accum)
+    } else
+      getPairs(x, div*10, accum)
+  }
+
+  val leftToRight = getPairs(p)
+  val rightToLeft = getPairs(p.toString.reverse.toInt)
 
   leftToRight ++ rightToLeft
 }
 
-def allConcatenations(N: Int)(implicit primes: ListSet[Int]) = {
+def allConcatenations(N: Int)(implicit primes: LinkedHashSet[Int]) = {
   primes.takeWhile(_ < N).flatMap(concatenations(_))
 }
 
-def isPrime(k: Int)(implicit p: ListSet[Int]) = {
+def isPrime(k: Int)(implicit p: LinkedHashSet[Int]) = {
   if(k < p.max) { 
     p(k)
-  } else if(k < p.max * p.max) {
+  } else if(k < p.max.toLong * p.max) {
     p.forall(k % _ != 0)
   } else sys.error(s"can't check: $k")
 
 }
 
-def check(chain: Seq[Int])(implicit p: ListSet[Int]) = 
+def check(chain: Seq[Int])(implicit p: LinkedHashSet[Int]) = 
   chain.combinations(2).forall { 
     case List(a,b) => 
-      isPrime((a.toString + b.toString).toInt) && 
-      isPrime((b.toString + a.toString).toInt)
+      isPrime(concat(a,b)) && isPrime(concat(b,a))
   }
 
 
-def chains(implicit maxLen: Int, symm: Map[Int, Seq[Int]], p: ListSet[Int]): Set[Seq[Int]] = {
+def chains(implicit maxLen: Int, symm: Map[Int, Seq[Int]], p: LinkedHashSet[Int]): Set[Seq[Int]] = {
   def chain(current: Seq[Int]): Seq[Seq[Int]] = {
     if(current.length == maxLen)
       return Seq(current)
@@ -80,7 +84,7 @@ def chains(implicit maxLen: Int, symm: Map[Int, Seq[Int]], p: ListSet[Int]): Set
   symm.keys.map(p => chain(Seq(p))).flatten.toSet
 }
 
-def lowestSum(k: Int)(implicit symm: Map[Int, Seq[Int]], p: ListSet[Int]): Int = {
+def lowestSum(k: Int)(implicit symm: Map[Int, Seq[Int]], p: LinkedHashSet[Int]): Int = {
 
   val all = chains(k/2+1, symm, p)
   val left = all.groupBy(ch => ch.last).mapValues(aSeq => aSeq.map(_.init))
@@ -104,7 +108,7 @@ def lowestSum(k: Int)(implicit symm: Map[Int, Seq[Int]], p: ListSet[Int]): Int =
     if check(chain)
     chainSum = chain.sum
     if chainSum < result
-  } { result = chainSum; lowestSeq = lc ++ (mid +: rc) }
+  } { result = chainSum; lowestSeq = lc ++ (mid +: rc); println("hit: " + lowestSeq.mkString(",")) }
 
   println("lowest seq: " + lowestSeq.mkString(","))
 
@@ -120,4 +124,5 @@ def solve(k: Int, maxP: Int, maxC: Int) {
 }
 
 solve(5, 2000000, 400000)
+//solve(4, 10000, 10000)
 
