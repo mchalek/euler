268adf56c0b0ba46905b3edf15681252479290b1
Kevin McHale <kevin@quantifind.com>
2014-02-07 08:16:36 -0500

    smarter pruning, small improvement, expected bigger

--- solved/p061/p61.scala
+++ solved/p061/p61.scala
@@ -7,20 +7,19 @@ def mkItems(f: Int => Int) = { n.map(f).filter(x => x > 999 && x < 10000) }
 def isCyclic(s: List[Int]) = // necessary but not sufficient
   s.forall(i => s.exists(j => i != j && j / 100 == i % 100) && s.exists(j => i != j && i / 100 == j % 100))
 
-def validifier(items: List[List[Int]]): List[List[Int]] = {
-    def validify(l0: List[Int], l1: Set[Int]) = {
-        val validSuffix = l1.map(x => x / 100)
-        val validPrefix = l1.map(x => x % 100)
-        l0.filter { x =>
-          validPrefix(x / 100) && validSuffix(x % 100)
-        }
+def validify(l0: List[Int], l1: Set[Int]) = {
+    val validSuffix = l1.map(x => x / 100)
+    val validPrefix = l1.map(x => x % 100)
+    l0.filter { x =>
+      validPrefix(x / 100) && validSuffix(x % 100)
     }
-
-    for {
-        l0 <- items
-        l1 = items.diff(List(l0)).flatten.toSet
-    } yield validify(l0, l1)
 }
+
+def validifier(items: List[List[Int]]): List[List[Int]] =
+    for {                                                                       
+        l0 <- items                                                             
+        l1 = items.diff(List(l0)).flatten.toSet                                 
+    } yield validify(l0, l1)                                                    
 
 def permer(items: List[List[Int]], cur: List[Int] = List.empty) {
   if(items.isEmpty) {
@@ -28,12 +27,11 @@ def permer(items: List[List[Int]], cur: List[Int] = List.empty) {
       println("%s => %d".format(cur.mkString(","), cur.sum))
     }
   } else {
+    val validItems = validify(items.head, (cur::(items.tail)).flatten.toSet)
     for {
-      q <- items.head if !cur.contains(q)
-      nextCur = q::cur 
-      reducedItems = validifier(nextCur::(items.tail)).tail
+      q <- validItems if !cur.contains(q)
     } {
-      permer(reducedItems, nextCur)
+      permer(items.tail, q::cur)
     }
   }
 }
@@ -46,9 +44,8 @@ val hep = mkItems(m => m*(5*m-3)/2)
 val oct = mkItems(m => m*(3*m-2))
 
 val items = List(tri,sq,pen,hex,hep,oct)
-val reduced = validifier(items)
 
-permer(reduced) // will print a bunch of solutions that are not correct because they contain sub-cycles.  manually check or whatever to find:
+permer(validifier(items)) // will print a bunch of solutions that are not correct because they contain sub-cycles.  manually check or whatever to find:
 //1281,2512,8128,2882,5625,8256 => 28684
 
 
