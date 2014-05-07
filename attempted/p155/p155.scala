aa37cf26e1ab186321a2f2f6ca5dc2788fc5edfa
Kevin McHale <kevin@quantifind.com>
2014-05-07 09:18:20 -0400

    p155 complete, a little slow but correct

--- attempted/p155/p155.scala
+++ attempted/p155/p155.scala
@@ -1,25 +1,53 @@
 import scala.collection.breakOut
+import scala.collection.immutable._
 
-case class Fraction(num: Long, den: Long) {
+case class Fraction(num: Long, den: Long) extends Ordered[Fraction] {
+  /*
   override def equals(that: Any) = {
     that match {
       case Fraction(n2, d2) => n2 * den == d2 * num
       case _ => false
     }
+  }*/
+
+  private def partialReduce = {
+    var newNum = num
+    var newDen = den
+
+    //val primes = Seq(2,3,5,7,11,13,17,19,23,29,31,37)
+    val primes = Seq(2,3,5,7,11)
+
+    primes.foreach { p =>
+      while(newNum % p == 0 && newDen % p == 0) {
+        newNum /= p
+        newDen /= p
+      }
+    }
+
+    Fraction(newNum, newDen)
   }
 
-  override def hashCode = math.round(1e3*num.toDouble / den).hashCode
+  def compare(that: Fraction): Int = {
+    val a = num * that.den
+    val b = den * that.num
+
+    if(a == b) 0 else if(a > b) 1 else -1
+  }
 
   def +(that: Fraction) = {
     val newNum = num * that.den + den * that.num
     val newDen = den * that.den 
     if(newNum % newDen == 0)
-      Fraction(newNum / newDen, 1)
+      Fraction(newNum / newDen, 1).partialReduce
+    else if (newDen % newNum == 0)
+      Fraction(1, newDen / newNum).partialReduce
     else
-      Fraction(newNum, newDen)
+      Fraction(newNum, newDen).partialReduce
   }
 
   def inv = Fraction(den, num)
+
+  override def toString = if(den == 1) num.toString else s"$num/$den"
 }
 
 object P155 {
@@ -30,18 +58,18 @@ object P155 {
   def iterate(
     n: Long,
     k: Long = 1,
-    counts: Map[Long, Set[Fraction]] = Map(1l -> Set(Fraction(60, 1)))): Set[Fraction] = {
+    counts: Map[Long, TreeSet[Fraction]] = Map(1l -> TreeSet(Fraction(60, 1)))): TreeSet[Fraction] = {
 
     println(s"current depth is $k")
 
     if(n == k)
-      return Set(Fraction(60, 1))
+      return TreeSet(Fraction(60, 1))
 
-    val current: Set[Fraction] = counts.flatMap { case (i, values) =>
-      val complementary = counts.getOrElse(k - i + 1, Set.empty[Fraction])
+    val current: TreeSet[Fraction] = counts.flatMap { case (i, values) =>
+      val complementary = counts.getOrElse(k - i + 1, Seq.empty[Fraction]).toSeq
 
       for {
-        v <- values
+        v <- values.toSeq
         c <- complementary
         newValue <- Seq(series(c, v), parallel(c, v))
       } yield newValue
@@ -54,18 +82,4 @@ object P155 {
 println(Seq(1, P155.iterate(1).size).mkString(" -> "))
 println(Seq(2, P155.iterate(2).size).mkString(" -> "))
 println(Seq(3, P155.iterate(3).size).mkString(" -> "))
-println(Seq(4, P155.iterate(4).size).mkString(" -> "))
-println(Seq(5, P155.iterate(5).size).mkString(" -> "))
-//println(Seq(6, P155.iterate(6).size).mkString(" -> "))
-//println(Seq(7, P155.iterate(7).size).mkString(" -> "))
-//println(Seq(8, P155.iterate(8).size).mkString(" -> "))
-//println(Seq(9, P155.iterate(9).size).mkString(" -> "))
-//println(Seq(10, P155.iterate(10).size).mkString(" -> "))
-//println(Seq(11, P155.iterate(11).size).mkString(" -> "))
-//println(Seq(12, P155.iterate(12).size).mkString(" -> "))
-//println(Seq(13, P155.iterate(13).size).mkString(" -> "))
-//println(Seq(14, P155.iterate(14).size).mkString(" -> "))
-//println(Seq(15, P155.iterate(15).size).mkString(" -> "))
-//println(Seq(16, P155.iterate(16).size).mkString(" -> "))
-//println(Seq(17, P155.iterate(17).size).mkString(" -> "))
 println(Seq(18, P155.iterate(18).size).mkString(" -> "))
