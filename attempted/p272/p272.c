0c75784afc7491d819c54a5b7191236c368222c0
Kevin McHale <kevin@quantifind.com>
2014-12-01 10:07:09 -0500

    collect primes

--- attempted/p272/p272.c
+++ attempted/p272/p272.c
@@ -1,6 +1,7 @@
 #include <prime.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 long cube(long x, long m)
 {
@@ -11,7 +12,7 @@ long cube(long x, long m)
 }
 
 #define MAX_X 15156
-#define MAX_P 1865706
+#define MAX_P 1865706l
 
 int main()
 {
@@ -19,6 +20,10 @@ int main()
     long *p;
 
     primes(MAX_P, &p, &np);
+
+    bool *p3hit = calloc(1 + MAX_P, sizeof(bool));
+    long *p3 = malloc(np*sizeof(long));
+    long np3 = 0;
 
     // look for values x^3 - 1 == np, for p <= 1865706
     // note: x^3 == 1 mod p => (x^2)^3 == 1 mod p so we only have to find
@@ -40,12 +45,19 @@ int main()
                 continue;
 
             if(e[j] == 1 && i < f[j]) {
+                if(!p3hit[f[j]]) {
+                    p3[np3++] = f[j];
+                    p3hit[f[j]] = true;
+                }
+
                 printf("%ld^3 == 1 mod %ld\n", i % f[j], f[j]);
                 printf("%ld^3 == 1 mod %ld\n", (i*i) % f[j], f[j]);
             }
         }
     }
 
+    free(p3hit);
+    printf("%ld primes below %ld with 3 cube roots of unity\n", np3, MAX_P);
 
     return 0;
 }
