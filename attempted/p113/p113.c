511dc861d5cbea493949000bba8c2662f8342269
Kevin McHale <mchalek@gmail.com>
2014-12-12 01:18:04 -0500

    fixes, now slightly over-counting

--- attempted/p113/p113.c
+++ attempted/p113/p113.c
@@ -5,35 +5,42 @@ int main()
 {
     long incr[10], decr[10];
 
-    int i, j;
+    int i, j, k;
 
     for(i = 1; i < 10; i++) {
         incr[i] = 1;
         decr[i] = 1;
     }
 
+    // first digit cannot be zero for either
     incr[0] = 0;
     decr[0] = 0;
 
-    for(i = 1; i < 6; i++) {
+    long result = 18;
+    for(i = 1; i < 10; i++) {
         long next_incr[10];
         long next_decr[10];
 
         next_incr[0] = incr[0];
-        for(j = 1; j < 10; j++)
-            next_incr[j] = incr[j] + incr[j-1];
+        for(j = 1; j < 10; j++) {
+            next_incr[j] = 0;
+            for(k = 0; k <= j; k++)
+                next_incr[j] += incr[k];
+        }
 
         next_decr[9] = decr[9];
-        for(j = 8; j >= 0; j--)
-            next_decr[j] = decr[j] + decr[j+1];
+        for(j = 8; j >= 0; j--) {
+            next_decr[j] = 0;
+            for(k = j; k < 10; k++)
+                next_decr[j] += decr[k];
+        }
 
         memcpy(incr, next_incr, sizeof(incr));
         memcpy(decr, next_decr, sizeof(decr));
-    }
 
-    long result = 0;
-    for(j = 0; j < 10; j++) {
-        result += incr[j] + decr[j];
+        for(j = 0; j < 10; j++) {
+            result += incr[j] + decr[j];
+        }
     }
 
     printf("result: %ld\n", result);
