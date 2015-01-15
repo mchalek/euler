fe2a8285bfd4c687f8b89f8d2071dfdefe29ee7a
Kevin McHale <kevin@quantifind.com>
2015-01-15 14:47:03 -0500

    threaded

--- attempted/p492/p492.c
+++ attempted/p492/p492.c
@@ -4,14 +4,68 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#include <pthread.h>
+
 #define X 1000000000l
 #define Y 1000l
 #define Z 1000000000000000l
 //#define Z 1000l
 
+#define NTHREADS 4
 #define WORK_SIZE (((X + Y + 63) / 64) * sizeof(uint64_t))
 
 #define NEXT(a) (6*(a)*(a) + 10*(a) + 3) // == (3a + 3)(2a + 1) + a
+
+typedef struct _tasks {
+    long *items;
+    long num_items;
+    long next_item;
+    pthread_mutex_t mutex;
+} tasks_t;
+
+long next(tasks_t *tasks) {
+    long ret = -1;
+    pthread_mutex_lock(&tasks->mutex);
+    if(tasks->next_item < tasks->num_items) {
+        ret = tasks->items[tasks->next_item];
+        tasks->next_item++;
+    }
+    pthread_mutex_unlock(&tasks->mutex);
+
+    return ret;
+}
+
+typedef struct _result {
+    long p;
+    long Bp;
+} result_t;
+
+typedef struct _results {
+    FILE *fp;
+    result_t *items;
+    int num_items;
+    pthread_mutex_t mutex;
+} results_t;
+
+long sum = 0;
+
+void update(results_t *results, long p, long Bp)
+{
+    result_t r;
+    r.p = p;
+    r.Bp = Bp;
+
+    pthread_mutex_lock(&results->mutex);
+    sum += Bp;
+    pthread_mutex_unlock(&results->mutex);
+}
+
+typedef struct _stuff {
+    tasks_t *tasks;
+    results_t *results;
+    uint64_t *work;
+} stuff_t;
+    
 
 bool check_and_update(long idx, uint64_t *work) {
     uint64_t word = idx / 64;
@@ -59,26 +113,66 @@ long B(long p, uint64_t *work) {
     return a;
 }
 
+void *worker(void *vstuff)
+{
+    stuff_t *stuff = (stuff_t *) vstuff;
+    tasks_t *tasks = stuff->tasks;
+    results_t *results = stuff->results;
+    uint64_t *work = stuff->work;
+
+    long p;
+    while((p = next(tasks)) != -1) {
+        long Bp = B(p, work);
+
+        update(results, p, Bp);
+    }
+
+    return NULL;
+}
+
 int main()
 {
     long *p, np;
     primes(100000, &p, &np);
 
     long i;
-    uint64_t *work = malloc(WORK_SIZE);
-    long result = 0;
-    for(i = X; i <= X+Y; i++) {
-        if(isprime(i, p, np)) {
-            printf("%ld is prime\n", i);
-            long Bi = B(i, work);
-            result += Bi;
-        } else {
-            //printf("%ld is not prime\n", i);
-        }
-    }
-    printf("B(%ld, %ld, %ld) == %ld\n", X, Y, Z, result);
 
-    free(p);
+    long nt = 0;
+    for(i = X; i <= X + Y; i++) {
+        if(isprime(i, p, np))
+            nt++;
+    }
+
+    tasks_t tasks;
+    tasks.num_items = 0;
+    tasks.items = malloc(nt*sizeof(long));
+    for(i = X; i <= X + Y; i++) {
+        if(isprime(i, p, np))
+            tasks.items[tasks.num_items++] = i;
+    }
+    tasks.next_item = 0;
+    pthread_mutex_init(&tasks.mutex, NULL);
+
+    results_t results;
+    pthread_mutex_init(&results.mutex, NULL);
+
+    pthread_t threads[NTHREADS];
+
+    for(i = 0; i < NTHREADS; i++) {
+        stuff_t *stuff = malloc(sizeof(stuff_t));
+
+        stuff->tasks = &tasks;
+        stuff->results = &results;
+        stuff->work = malloc(WORK_SIZE);
+
+        pthread_create(threads + i, NULL, worker, stuff);
+    }
+
+    for(i = 0; i < NTHREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("sum: %ld\n", sum);
 
     return 0;
 }
