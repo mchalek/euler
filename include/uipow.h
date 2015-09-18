619333ef6ced3f6cb1777e507eeb93fce73a69b2
Kevin McHale <kevin@quantifind.com>
2015-09-18 10:18:49 -0400

    track down overflow in uipow'

--- include/uipow.h
+++ include/uipow.h
@@ -16,27 +16,41 @@ static inline uint64_t uipow(uint64_t a, uint64_t b, uint64_t modulus)
     if(b == 1)
         return a % modulus;
 
-    if((a % modulus) >= (1ul << 32)) {
-        fprintf(stderr, "ERROR: %ld %% %ld is too large, cannot exponentiate!\n",
-                a, modulus);
-        exit(-1);
-    }
-
     a %= modulus;
 
-    uint64_t a2n = a; // a^(2^n), starting with n == 0
-    uint64_t y = 1ul;
     uint64_t result = 1ul;
-    do {
-        if(b & y) {
-            result *= a2n;
-            result %= modulus;
-            b ^= y;
-        }
-        a2n *= a2n;
-        a2n %= modulus;
-        y <<= 1;
-    } while(b);
+    uint64_t y = 1ul;
+    if(modulus < (1ul << 32)) {
+        uint64_t a2n = a; // a^(2^n), starting with n == 0
+        do {
+            if(b & y) {
+                result *= a2n;
+                result %= modulus;
+                b ^= y;
+            }
+            a2n *= a2n;
+            a2n %= modulus;
+            y <<= 1;
+        } while(b);
+    } else {
+        // overflows will happen on uint64_t's
+        // use gcc's builtin support for uint128
+        __uint128_t result128 = 1ul;
+        __uint128_t a2n = a; // a^(2^n) mod modulus, starting with n == 0
+        do {
+            if(b & y) {
+                result128 *= a2n;
+                result128 %= modulus;
+                b ^= y;
+            }
+            a2n *= a2n;
+            a2n %= modulus;
+            y <<= 1;
+        } while(b);
+
+        // result is guaranteed to fit in 64 bits
+        result = (uint64_t) result128;
+    }
 
     return result;
 }
