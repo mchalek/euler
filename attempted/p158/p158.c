f78ad330a7884b80bfb6dab84116264e25f6377b
Kevin McHale <mchalek@gmail.com>
2015-09-19 10:54:49 -0400

    test all pass for fixed-density exhauster with indexing

--- attempted/p158/p158.c
+++ attempted/p158/p158.c
@@ -26,7 +26,7 @@ typedef struct {
     long count_without[26];
 } counts_t;
 
-int _efd(int num_bits, int remaining, int min_bit, uint32_t current, int *count, counts_t *items, bool invert) {
+int _efd(int num_bits, int remaining, int min_bit, uint32_t current, int *count, counts_t *items, int index[], bool invert) {
     // generate all bit fields of length num_bits and density given by
     // the initial value of the remaining field.
     // Accumulate those in the items array, which must be pre-allocated to
@@ -36,6 +36,8 @@ int _efd(int num_bits, int remaining, int min_bit, uint32_t current, int *count,
         memset(items + *count, 0, sizeof(counts_t));
         if(invert)
             current ^= ((1 << num_bits) - 1);
+
+        index[current] = *count;
 
         items[*count].mask = current;
         (*count)++;
@@ -49,30 +51,36 @@ int _efd(int num_bits, int remaining, int min_bit, uint32_t current, int *count,
         if(current & bit)
             continue;
         
-        result += _efd(num_bits, remaining - 1, 
-                pos + 1, current ^ bit, count, items, invert);
+        result += _efd(num_bits, remaining - 1, pos + 1, current ^ bit,
+                count, items, index, invert);
     }
 
     return result;
 }
 
-int exhaust_fixed_density(int num_bits, int density, counts_t *items) {
+int exhaust_fixed_density(int num_bits, int density, counts_t *items, int index[]) {
     int num_hits = 0;
+
+    int i;
+    for(i = 0; i < (1 << num_bits); i++) {
+        index[i] = -1;
+    }
     
     if(num_bits - density < density) {
-        return _efd(num_bits, num_bits-density, 0, 0, &num_hits, items, true);
+        return _efd(num_bits, num_bits-density, 0, 0, &num_hits, items, index, true);
     } else {
-        return _efd(num_bits, density, 0, 0, &num_hits, items, false);
+        return _efd(num_bits, density, 0, 0, &num_hits, items, index, false);
     }
 }
 
 int main(void) {
-    int density = 16;
+    int density = 13;
     int expected = nchoosek(26, density);
     printf("expected items: %d\n", expected);
     counts_t *items = calloc(expected, sizeof(counts_t));
+    int *index = malloc((1 << 26)*sizeof(int));
 
-    int num_items = exhaust_fixed_density(26, density, items);
+    int num_items = exhaust_fixed_density(26, density, items, index);
 
     printf("items generated: %d\n", num_items);
 
