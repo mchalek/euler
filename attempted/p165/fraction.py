bd9f263a583a32191dedb173e5f96d4d8478d9b4
Kevin McHale <kevin@quantifind.com>
2015-09-30 14:01:00 -0400

    python attempt at p165, including ridiculous fraction class

---
+++ attempted/p165/fraction.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python
+
+def get_primes(N):
+    composites = set([])
+    primes = [2]
+
+    p = 1
+    while p < N:
+        p += 2
+        if p in composites:
+            continue
+
+        primes.append(p)
+
+        q = 2*p
+        while q < N:
+            composites.add(q)
+            q += p
+
+    return primes
+
+class Factorer:
+    def __init__(self):
+        self.max_prime = 10000000
+        self.max_factor = self.max_prime**2
+        self.primes = get_primes(self.max_prime)
+
+    def factor(self, n):
+        if n > self.max_factor:
+            raise Exception('Cannot factor %d: too big!!' % n)
+
+        n0 = n
+        factors = {}
+        if n is 0:
+            return factors
+
+        factors[1] = 1
+
+        ip = 0
+        while n > 1 and ip < len(self.primes):
+            p = self.primes[ip]
+    
+            e = 0
+            while 0 == (n % p):
+                e += 1
+                n /= p
+
+            if e is not 0:
+                factors[p] = e
+
+            if (1+p)*(1+p) > n:
+                break
+
+            ip += 1
+
+        if n > 1:
+            factors[n] = 1
+
+        return factors
+
+factorer = Factorer()
+
+class Fraction:
+    def __init__(self, num, den, negative=False):
+        if type(num) == dict and type(den) == dict:
+            self.negative = negative
+            self.fn = num
+            self.fd = den
+        else:
+            if num * den < 0:
+                self.negative = not negative
+            else:
+                self.negative = negative
+
+            if num < 0:
+                num = -num
+
+            if den < 0:
+                den = -den
+
+            self.fn = factorer.factor(num)
+            self.fd = factorer.factor(den)
+
+    def reduce(self):
+        k0 = set(self.fn.keys())
+        k1 = set(self.fd.keys())
+
+        common = k0 & k1
+
+        for p in common:
+            if p == 1: # don't discard the 1, needed to distinguish 1 from 0
+                continue
+
+            nn = self.fn.pop(p)
+            nd = self.fd.pop(p)
+
+            if nn > nd:
+                self.fn[p] = nn - nd
+            elif nn < nd:
+                self.fd[p] = nd - nn
+
+    def prod(self, factors):
+        if len(factors) == 0:
+            return 0
+        else:
+            return reduce(lambda x, y: x * y, [p**e for (p, e) in factors.items()])
+
+    def __str__(self):
+        return '%d/%d' % self.tuple()
+
+    def __repr__(self):
+        return self.__str__()
+
+    def factored_multiply(self, a, b):
+        product = {p:e for (p, e) in a.items()}
+        for (p, e) in b.items():
+            prev = product.get(p, 0)
+            product[p] = e + prev
+
+        return product
+
+    def tuple(self):
+        self.reduce()
+        if self.negative:
+            numerator = -self.prod(self.fn)
+        else:
+            numerator = self.prod(self.fn)
+
+        denominator = self.prod(self.fd)
+
+        return (numerator, denominator)
+
+    def __add__(self, that):
+        if type(that) == int:
+            return self.__add__(Fraction(that, 1))
+        
+        new_fd = self.factored_multiply(self.fd, that.fd)
+
+        A = self.prod(self.factored_multiply(self.fn, that.fd))
+        if self.negative:
+            A = -A
+
+        B = self.prod(self.factored_multiply(that.fn, self.fd))
+        if that.negative:
+            B = -B
+
+        new_num = A + B
+        new_neg = new_num < 0
+        if new_neg:
+            new_num = -new_num
+
+        new_fn = factorer.factor(new_num)
+
+        return Fraction(new_fn, new_fd, new_neg)
+
+    def __mul__(self, that):
+        if type(that) == int:
+            return self.__mul__(Fraction(that, 1))
+
+        if len(self.fn) == 0 or len(that.fn) == 0:
+            return Fraction(0,1)
+
+        new_fn = {p:e for (p, e) in self.fn.items()}
+        for (p, e) in that.fn.items():
+            prev = new_fn.get(p, 0)
+            new_fn[p] = e + prev
+
+        new_fd = {p:e for (p, e) in self.fd.items()}
+        for (p, e) in that.fd.items():
+            prev = new_fd.get(p, 0)
+            new_fd[p] = e + prev
+
+        new_negative = self.negative ^ that.negative
+
+        return Fraction(new_fn, new_fd, new_negative)
+
+    def __rmul__(self, that):
+        return self.__mul__(that)
+
+    def __radd__(self, that):
+        return self + that
+
+    def __sub__(self, that):
+        return self + (that * -1)
+
+    def __rsub__(self, that):
+        return (self * -1) + that
+
+    def inverse(self):
+        return Fraction(self.fd, self.fn)
+
+    def div(self, that):
+        if type(that) == int:
+            return self * Fraction(1, that)
+
+        return self * that.inverse()
+
+    def __lt__(self, that):
+        if type(that) == int:
+            return self < Fraction(that, 1)
+
+        (n0,d0) = self.tuple()
+        (n1,d1) = that.tuple() 
+
+        return n0*d1 < d0*n1
+
+    def __eq__(self, that):
+        if type(that) == int:
+            return self == Fraction(that, 1)
+
+        return self.tuple() == that.tuple()
+
+    def __gt__(self, that):
+        if type(that) == int:
+            return Fraction(that, 1) < self
+
+        return that < self
+
+if __name__ == '__main__':
+    f0 = Fraction(10, 30)
+    f1 = Fraction(8, 10)
+
+    print(f0)
+    print(f1)
+    print('%s - 1: %s' % (str(f0), str(f0-1)))
+    print('1 - %s: %s' % (str(f0), str(1-f0)))
+    f2 = f0 * f1
+    print('%s * %s == %s' % (str(f0), str(f1), str(f2)))
+    f3 = f0 + f1
+    print('%s + %s == %s' % (str(f0), str(f1), str(f3)))
+    f4 = f1 - f0 + Fraction(3,16)
+    print('%s - %s == %s' % (str(f1), str(f0), str(f4)))
+
+    print('sum of all of them: %s' % str(sum([f0,f1,f2,f3,f4])))
+
---
+++ attempted/p165/p165.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+from fraction import Fraction
+
+class LineSegment:
+    def __init__(self, p0, p1):
+        (a, b) = p0
+        (c, d) = p1
+
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+def check(l0, l1):
+    a = l0.a
+    b = l0.b
+    c = l0.c
+    d = l0.d
+
+    e = l1.a
+    f = l1.b
+    g = l1.c
+    h = l1.d
+
+    num = (c-g)*(f-h) - (d-h)*(e-g)
+    den = (b-d)*(e-g) - (a-c)*(f-h)
+
+    if den == 0:
+        return None # no intersection points
+
+    t = float(num) / den
+
+    #print('t == %s; tf == %s' % (str(t), str(tf)))
+
+    if 0 < t < 1:
+        if e == g:
+            s = (t * (b-d) + (d-h)) / (f-h)
+        else:
+            s = (t * (a-c) + (c-g)) / (e-g)
+
+        #print('s == %s; sf == %s' % (str(s), str(sf)))
+
+        if 0 < s < 1:
+#            tf = Fraction(num, den)
+#            if e == g:
+#                sf = (tf * (b-d) + (d-h)) * Fraction(1, f-h)
+#            else:
+#                sf = (tf * (a-c) + (c-g)) * Fraction(1, e-g)
+
+            #if 0 < sf < 1 and 0 < tf < 1:
+            # compute intersection point and return
+            x = t*a + (1-t)*c
+            y = t*b + (1-t)*d 
+            return (x, y)
+
+            #xf = tf*a + (1-tf)*c
+            #yf = tf*b + (1-tf)*d 
+            #return (xf.tuple(), yf.tuple())
+
+    return None
+
+def generate(N):
+    items = []
+    s4 = 290797
+
+    for i in range(N):
+        s1 = (s4*s4) % 50515093
+        s2 = (s1*s1) % 50515093
+        s3 = (s2*s2) % 50515093
+        s4 = (s3*s3) % 50515093
+
+        if i == 0:
+            print('(%d, %d), (%d, %d)' % (s1 % 500, s2 % 500, s3 % 500, s4 % 500))
+
+        segment = LineSegment((s1 % 500, s2 % 500), (s3 % 500, s4 % 500))
+        items.append(segment)
+
+    return items
+
+segments = generate(5000)
+
+p1 = LineSegment((27, 44), (12, 32))
+p2 = LineSegment((46, 53), (17, 62))
+p3 = LineSegment((46, 70), (22, 40))
+
+print('check p1, p2: ' + str(check(p1, p2)))
+print('check p2, p3: ' + str(check(p2, p3)))
+print('check p1, p3: ' + str(check(p1, p3)))
+
+intersections = set([])
+num_raw = 0
+for i in range(len(segments)):
+    if i % 100 == 0:
+        print('working on segment %d' % i)
+    a = segments[i]
+    for j in range(1 + i, len(segments)):
+        b = segments[j]
+
+        x = check(a, b)
+
+        if x is not None:
+            num_raw += 1
+            intersections.add(x)
+
+print('total number of intersections: %d' % num_raw)
+print('number of unique intersections: %d' % len(intersections))
+
