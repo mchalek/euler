df334e18f207c637f288aefe27611c5e796d4281
Kevin McHale <kevin@quantifind.com>
2015-09-30 14:00:15 -0400

    fraction reducer method

--- libs/primes/prime.h
+++ libs/primes/prime.h
@@ -11,6 +11,8 @@ long nlogi(long n, long i);
 long ipow(long x, long y);
 void factor(long x, long primes[], long np, long *nfactors, long pfactors[], long exponents[]);
 
+void reduce(long n, long d, long np, long p[], long *n_red, long *d_red);
+
 long num_proper_divisors(long nf, long exponents[]);
 void divisors(long nf, long factors[], long exponents[], long *nd, long **divisors_out);
 void bounded_divisors(long min, long max, long nf, long factors[], long exponents[], long *nd_out, long **divisors_out);
