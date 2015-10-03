efa7f0705c0e5f5c1104fa88828245cad2b6efe5
Kevin McHale <kevin@quantifind.com>
2015-10-03 15:33:53 -0400

    bug fixes, comments

--- attempted/p178/p178.c
+++ attempted/p178/p178.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// dynamic programming, essentially, keeping track of 4 types of numbers:
+//  - those that contain neither 0 nor 9
+//  - those that contain 9 but not 0
+//  - those that contain 0 but not 9
+//  - those that contain both 9 and 0
+//
+//  Note that due to these being "step numbers", the final condition
+//  is necessary and sufficient for being pandigital
+
 void initialize(long h0[], long h9[], long hn[], long pd[]) {
     hn[1] = 1;
     hn[2] = 1;
@@ -13,8 +22,14 @@ void initialize(long h0[], long h9[], long hn[], long pd[]) {
     h9[9] = 1;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    if(argc < 2) {
+        printf("SYNTAX: %s ndig [ndig == 40 solves problem]\n", argv[0]);
+        exit(0);
+    }
+    int max_length = atoi(argv[1]);
+
     long *hit_0 = calloc(10, sizeof(long));
     long *hit_9 = calloc(10, sizeof(long));
     long *hit_nil = calloc(10, sizeof(long));
@@ -24,7 +39,7 @@ int main(void)
 
     int current_length;
     long result = 0;
-    for(current_length = 1; current_length < 40; current_length++) {
+    for(current_length = 1; current_length < max_length; current_length++) {
         int j;
         long *new_hit_0 = calloc(10, sizeof(long));
         long *new_hit_9 = calloc(10, sizeof(long));
@@ -33,7 +48,7 @@ int main(void)
 
         // first update hit_nil -> X
         new_hit_0[0] += hit_nil[1];
-        new_hit_nil[0] += hit_nil[1];
+        new_hit_nil[2] += hit_nil[1];
         for(j = 2; j < 8; j++) {
            new_hit_nil[j-1] += hit_nil[j]; 
            new_hit_nil[j+1] += hit_nil[j]; 
@@ -67,8 +82,7 @@ int main(void)
         }
         new_pandigital[8] += pandigital[9];
 
-        printf("result at length %d: %ld\n", current_length, result);
-        for(j = 0; j < 9; j++) {
+        for(j = 0; j < 10; j++) {
             result += new_pandigital[j];
         }
 
