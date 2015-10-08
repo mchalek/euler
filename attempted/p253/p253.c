8bbe2018f78b1c7d1088f0533a26d2ab4c3d1a63
Kevin McHale <mchalek@gmail.com>
2015-10-08 02:10:50 -0400

    terrible simulator for p253.  dont even get me started but its 2am

--- attempted/p253/p253.c
+++ attempted/p253/p253.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include <kmbits.h>
+
+#define NUM_PIECES 40
 
 static inline int count_pieces(uint64_t config) {
     int result = 0;
@@ -12,8 +16,48 @@ static inline int count_pieces(uint64_t config) {
     return result;
 }
 
+void random_shuffle(int items[]) {
+    int i;
+    for(i = 0; i < NUM_PIECES-1; i++) {
+        int ind = i + (rand() % (NUM_PIECES-i));
+        int tmp = items[ind];
+        items[ind] = items[i];
+        items[i] = tmp;
+    }
+}
+
+int execute() {
+    int i;
+    int pieces[NUM_PIECES];
+    for(i = 0; i < NUM_PIECES; i++)
+        pieces[i] = i;
+
+    random_shuffle(pieces);
+
+    uint64_t state = 0;
+    int max_count = 0;
+    for(i = 0; i < NUM_PIECES; i++) {
+        state |= 1ul << pieces[i];
+        int count = count_pieces(state);
+
+        if(count > max_count) {
+            max_count = count;
+        }
+    }
+
+    return max_count;
+}
+
+
 int main(void) {
-    printf("pieces %lx == %d\n", 0xffaaccbbddl, count_pieces(0xffaaccbbddl));
+    int i;
+    double sum = 0.0;
+    int num_trials = 10000000;
+    srand(time(NULL));
+    for(i = 0; i < num_trials; i++) {
+        sum += execute();
+    }
+    printf("mean by shitty simulation: %.6f\n", sum / num_trials);
     
     return 0;
 }
