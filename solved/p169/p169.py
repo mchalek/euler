1c51c1dd9fc7b3cb7bfce9fcb23c626a01bc9a9b
Kevin McHale <mchalek@gmail.com>
2015-10-20 12:18:13 -0400

    just solve it in python

--- solved/p169/p169.py
+++ solved/p169/p169.py
@@ -14,14 +14,26 @@ def base2(n):
     p -= 1
     return [p] + base2(n - z)
 
+def count(n):
+    powers = base2(n)
+    num_powers = len(powers)
+    intermediates = [0]*len(powers)
+
+    intermediates[num_powers-1] = powers[num_powers-1] + 1
+    for i in range(num_powers - 2, -1, -1):
+        intermediates[i] += intermediates[i+1]*(powers[i]-powers[i+1])
+        for j in range(i+1, num_powers-1):
+            intermediates[i] += intermediates[j+1]*(powers[j]-powers[j+1]-1)
+        intermediates[i] += powers[num_powers-1]
+
+    return intermediates[0]
+
+
 if len(argv) < 2:
     print('No N provided; assuming default value of 10^25')
     N = 10**25
 else:
     N = int(argv[1])
 
-powers = base2(N)
-
-args = ['./p169'] + map(str, powers)
-subprocess.call(args)
+print('result: %d' % count(N))
 
