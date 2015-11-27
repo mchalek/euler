9d8a982901ef7087f32e2fcc458187046a1e5d3f
Kevin McHale <mchalek@gmail.com>
2015-11-27 14:23:57 -0500

    wip

--- attempted/p126/p126.scala
+++ attempted/p126/p126.scala
@@ -1,27 +1,38 @@
 import collection.mutable
 
-class Cube(val x: Int, val y: Int, val z: Int, faceCovered: Seq[Boolean] = Seq.fill(6)(false)) {
+class Cube(coords: (Int, Int, Int), faceCovered: Seq[Boolean] = Seq.fill(6)(false)) {
   require(faceCovered.length == 6, "Invalid faceCovered specification!  Must have 6 entries")
 
-  def cover = {
+  val _faceCovered = faceCovered.toArray
+
+  def setCover(ind: Int) {
+    _faceCovered(ind) = true
+  }
+
+  def x = coords._1
+  def y = coords._2
+  def z = coords._3
+
+
+  def coverLayer = {
     val result = mutable.Buffer.empty[Cube]
-    if(!faceCovered(0)) {
-      result += new Cube(1 + x, y, z, Seq(false, false, false, false, false, true))
+    if(!_faceCovered(0)) {
+      result += new Cube((1 + x, y, z), Seq(false, false, false, false, false, true))
     }
-    if(!faceCovered(1)) {
-      result += new Cube(x, y, 1 + z, Seq(false, false, false, true, false, false))
+    if(!_faceCovered(1)) {
+      result += new Cube((x, y, 1 + z), Seq(false, false, false, true, false, false))
     }
-    if(!faceCovered(2)) {
-      result += new Cube(x, 1 + y, z, Seq(false, false, false, false, true, false))
+    if(!_faceCovered(2)) {
+      result += new Cube((x, 1 + y, z), Seq(false, false, false, false, true, false))
     }
-    if(!faceCovered(3)) {
-      result += new Cube(x, y, z - 1, Seq(false, true, false, false, false, false))
+    if(!_faceCovered(3)) {
+      result += new Cube((x, y, z - 1), Seq(false, true, false, false, false, false))
     }
-    if(!faceCovered(4)) {
-      result += new Cube(x, y - 1, z, Seq(false, false, true, false, false, false))
+    if(!_faceCovered(4)) {
+      result += new Cube((x, y - 1, z), Seq(false, false, true, false, false, false))
     }
-    if(!faceCovered(5)) {
-      result += new Cube(x - 1, y, z, Seq(true, false, false, false, false, false))
+    if(!_faceCovered(5)) {
+      result += new Cube((x - 1, y, z), Seq(true, false, false, false, false, false))
     }
 
     result.toSeq
@@ -41,35 +52,49 @@ class Cube(val x: Int, val y: Int, val z: Int, faceCovered: Seq[Boolean] = Seq.f
   override def hashCode = {
     (x, y, z).hashCode
   }
+
+  def +(that: Cube) = {
+    for {
+      (isCovered, ind) <- that._faceCovered.zipWithIndex if isCovered
+    } {
+      this.setCover(ind)
+    }
+
+    this
+  }
 }
 
 object CubeHelper {
   def merge(cubes: Seq[Cube]): Seq[Cube] = {
-    val cubeCoordSet = cubes.map(cube => (cube.x, cube.y, cube.z)).toSet
+    val (cubeCoords, layer) = cubes.groupBy(cube => (cube.x, cube.y, cube.z)).toSeq.map { 
+      case (coord, cubes) =>
+        (coord, cubes.reduce(_+_))
+    }.unzip
 
-    cubeCoordSet.toSeq.map { case (x, y, z) =>
-      val newCovered = Seq(
-        cubeCoordSet.contains((x + 1, y, z)),
-        cubeCoordSet.contains((x, y, z + 1)),
-        cubeCoordSet.contains((x, y + 1, z)),
-        cubeCoordSet.contains((x, y, z - 1)),
-        cubeCoordSet.contains((x, y - 1, z)),
-        cubeCoordSet.contains((x - 1, y, z)))
+    val cubeCoordSet = cubeCoords.toSet
 
-      new Cube(x, y, z, newCovered)
+    layer.toSeq.foreach { cube =>
+      val (x, y, z) = (cube.x, cube.y, cube.z)
+      if(cubeCoordSet.contains((x + 1, y, z))) cube.setCover(0)
+      if(cubeCoordSet.contains((x, y, z + 1))) cube.setCover(1)
+      if(cubeCoordSet.contains((x, y + 1, z))) cube.setCover(2)
+      if(cubeCoordSet.contains((x, y, z - 1))) cube.setCover(3)
+      if(cubeCoordSet.contains((x, y - 1, z))) cube.setCover(4)
+      if(cubeCoordSet.contains((x - 1, y, z))) cube.setCover(5)
     }
+
+    layer
   }
 }
 
-class Solid(cubes: Seq[Cube], innerVolume: Int = 0) {
+class Solid(cubes: Seq[Cube]) {
   def volume = cubes.length
-  def outerLayer = volume - innerVolume
 
   def nextLayer = {
-    val nextCubes = cubes.flatMap(_.cover)
-    val merged = CubeHelper.merge(cubes ++ nextCubes)
+    val nextCubes = cubes.flatMap(_.coverLayer)
+    val merged = CubeHelper.merge(nextCubes)
 
-    new Solid(merged, volume)
+    new Solid(merged)
   }
 
   override def toString = {
@@ -99,28 +124,28 @@ object Solid {
     } {
       val covered = Seq(isCovered(x, false, nx), isCovered(z, false, nz), isCovered(y, false, ny),
         isCovered(z, true, nz), isCovered(y, true, ny), isCovered(x, true, nx))
-      cubes += new Cube(x, y, z, covered)
+      cubes += new Cube((x, y, z), covered)
     }
 
     new Solid(cubes.toSeq)
   }
 }
 
-val MAX_N = 1600
+val MAX_N = 200
 val counts = mutable.Map.empty[Int, Int]
 var x = 1
 var solid = Solid(1,1,1)
-while(solid.outerLayer <= MAX_N) {
+while(solid.volume <= MAX_N) {
   var y = 1
-  while(y <= x && solid.outerLayer <= MAX_N) {
+  while(y <= x && solid.volume <= MAX_N) {
     var z = 1
-    while(z <= y && solid.outerLayer <= MAX_N) {
-      //println(s"Solid ($x, $y, $z) => ${solid.outerLayer}")
+    while(z <= y && solid.volume <= MAX_N) {
+      //println(s"Solid ($x, $y, $z) => ${solid.volume}")
       var layer = 0
-      while(solid.outerLayer < MAX_N) {
+      while(solid.volume < MAX_N) {
         if(layer > 0) {
-          val prev_count = counts.getOrElseUpdate(solid.outerLayer, 0)
-          counts(solid.outerLayer) += 1
+          val prev_count = counts.getOrElseUpdate(solid.volume, 0)
+          counts(solid.volume) += 1
         }
 
         layer += 1
