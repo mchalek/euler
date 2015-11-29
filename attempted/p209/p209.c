d8f64e1813e46eaae686ce14f61bb4e856774a4b
Kevin McHale <mchalek@gmail.com>
2015-11-29 18:54:54 -0500

    possible solution to p209?

--- attempted/p209/p209.c
+++ attempted/p209/p209.c
@@ -8,16 +8,11 @@
  * There are 64 possible inputs (a, b, c, d, e, f).
  * - The transformation specified, t(a,b,c,d,e,f) => t(b,c,d,e,f,a ^ (b & c)),
  *   defines a permutation of the inputs (i.e. it is 1-1)
- * - Let I be an input, and pI be the permuted input.  The problem constraint
- *   t(I) & t(pI) == 0 requires t(I) == 0 OR t(pI) == 0 (OR both)
- * - Consider counting tables for which the converse is true,
- *   t(I) == 1 AND t(pI) == 1, for any I.  Suppose t(I) == 1 and t(pI) == 1,
- *   for some I.  Then we also have that t(p^2I) == 1, t(p^3I) == 1, ...
- *   So we can partition the inputs into cosets/equivalence classes and
- *   exhaust over possible values of these.
- * - Can compute the total count by inclusion/exclusion or something like that?
- *   e.g. Let C_j be a coset.  Then
- *   N == 2^64 - |Union_j { (# tables with C_j == 1) }|
+ * - This permutation has a few cycles in it.  We can study the cycles
+ *   independently, but one of them is especially big (46 items long)
+ *   This is 23 pairs, each with 3 possible values, but certain values in
+ *   some pairs rule out values in others...
+ * - Is this equivalent to counting bit strings where no 2 adjacent entries are 1?
  */
 
 int transform(int x) {
@@ -85,6 +80,32 @@ long build(uint64_t set, uint64_t word, int map[64], int depth) {
     return ret;
 }
 
+long cycle_count(int length) {
+    // All we need is the length of the cycle, we want to count bit strings
+    // where no 2 adjacent entries are 1
+    
+    long count00 = 1; // # of sequences starting at 0, with terminal entry 0
+    long count01 = 0; // # of sequences starting at 0, with terminal entry 1
+    long count10 = 0; // # of sequences starting at 1, with terminal entry 0
+    long count11 = 1; // # of sequences starting at 1, with terminal entry 1
+
+    int i;
+    for(i = 1; i < length; i++) {
+        long tmp00 = count01 + count00;
+        long tmp01 = count00;
+        long tmp10 = count11 + count10;
+        long tmp11 = count10;
+
+        count00 = tmp00;
+        count01 = tmp01;
+        count10 = tmp10;
+        count11 = tmp11;
+    }
+
+    // must discard count11 because it's invalid
+    return count00 + count01 + count10;
+}
+
 int main(void) {
     int map[64];
     int i;
@@ -93,6 +114,7 @@ int main(void) {
         map[i] = transform(i);
     }
 
+    long result = 1;
     int n_cycles = 0;
     unsigned char cycle_ind[64];
     memset(cycle_ind, 0xff, sizeof(cycle_ind));
@@ -100,25 +122,34 @@ int main(void) {
         if(cycle_ind[i] != 0xff)
             continue;
 
+        int cycle_length = 1;
         cycle_ind[i] = (unsigned char) n_cycles;
         int j = map[i];
         printf("cycle %d: %2d => %2d", n_cycles, i, j);
         while(j != i) {
+            if(j != i)
+                cycle_length++;
             cycle_ind[j] = (unsigned char) n_cycles;
             j = map[j];
             printf(" => %2d", j);
         }
-        printf("\n");
+
+
+        long nseq = cycle_count(cycle_length);
+        printf(" (length is %d, %ld valid sequences)\n", cycle_length, nseq);
+
+        result *= nseq;
 
         n_cycles++;
     }
 
+    /*
     for(i = 0; i < 64; i++) {
         printf("H(%d) == %d (cycle index %d)\n", i , map[i], (int) cycle_ind[i]);
-    }
+    }*/
 
     //long count = build(0, 0, map, 0);
 
-    //printf("result: %ld\n", count);
+    printf("result: %ld\n", result);
     return 0;
 }
