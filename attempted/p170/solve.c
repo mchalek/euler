7d4c492ec69c7aaa026adf6605cfda22e767ca41
Kevin McHale <mchalek@gmail.com>
2016-12-11 19:30:29 -0500

    solved p170 for 1 + 2 integers; must solve for 1 + (2 _or more_)

--- attempted/p170/solve.c
+++ attempted/p170/solve.c
@@ -30,9 +30,9 @@ int mask_to_digits(int mask, int digits[]) {
 
 void build_permutations(int digits_used_mask, int perm, int perm_len, int ndig, int digits[], int *nperm, int permutations[]) {
     if(perm_len == ndig) {
-        //permutations[(*nperm)++] = perm;
-        (*nperm)++;
-        printf("%d\n", perm);
+        if(perm) {
+            permutations[(*nperm)++] = perm;
+        }
         return;
     }
 
@@ -69,7 +69,19 @@ bool test(long product, int *mask_out) {
         product /= 10;
     }
 
+    *mask_out = mask;
     return true;
+}
+
+long concat(long a, long b) {
+    long tmp = b;
+
+    while(tmp) {
+        a *= 10;
+        tmp /= 10;
+    }
+
+    return a + b;
 }
 
 void build_from_masks(int ma, int mb, int mc) {
@@ -95,46 +107,47 @@ void build_from_masks(int ma, int mb, int mc) {
     build_permutations(0, 0, 0, ndig_b, dig_b, &n_perm_test_b, perms_b);
     build_permutations(0, 0, 0, ndig_c, dig_c, &n_perm_test_c, perms_c);
 
-    if(n_perm_test_a != n_perm_a) {
-        fprintf(stderr, "ERROR: n_perm_test_a (%d) != n_perm_a (%d)\n", n_perm_test_a, n_perm_a);
-        exit(1);
-    }
-    if(n_perm_test_b != n_perm_b) {
-        fprintf(stderr, "ERROR: n_perm_test_b (%d) != n_perm_b (%d)\n", n_perm_test_b, n_perm_b);
-        exit(1);
-    }
-    if(n_perm_test_c != n_perm_c) {
-        fprintf(stderr, "ERROR: n_perm_test_c (%d) != n_perm_c (%d)\n", n_perm_test_c, n_perm_c);
-        exit(1);
-    }
-
     int a, b, c;
-    for(a = 0; a < n_perm_a; a++) {
+    for(a = 0; a < n_perm_test_a; a++) {
         int perm_a = perms_a[a];
-        for(b = 0; b < n_perm_b; b++) {
+
+        for(b = 0; b < n_perm_test_b; b++) {
             int perm_b = perms_b[b];
+
             long prod_b = ((long) perm_a) * perm_b;
             int mask_b;
 
             if(!test(prod_b, &mask_b)) {
+                //printf("bad product: %ld\n", prod_b);
                 continue;
             }
 
-            for(c = 0; c < n_perm_c; c++) {
+            for(c = 0; c < n_perm_test_c; c++) {
                 int perm_c = perms_c[c];
                 int mask_c;
                 long prod_c = ((long) perm_a) * perm_c;
 
                 if(!test(prod_c, &mask_c)) {
+                    //printf("bad product: %ld\n", prod_c);
                     continue;
                 }
 
                 if(mask_b & mask_c) {
+                    //printf("conflicting products: %ld %ld\n", prod_b, prod_c);
                     continue;
                 }
+                
+                //printf("X(%d | %d; %d): %ld%ld <=> %ld%ld\n", perm_a, perm_b, perm_c, prod_b, prod_c, prod_c, prod_b);
+
 
                 if((mask_b | mask_c) == ((1 << 10) - 1)) {
                     printf("CP(%d | %d; %d): %ld%ld <=> %ld%ld\n", perm_a, perm_b, perm_c, prod_b, prod_c, prod_c, prod_b);
+
+                    long bc = concat(prod_b, prod_c);
+                    long cb = concat(prod_c, prod_b);
+
+                    long larger = (bc > cb) ? bc : cb;
+                    largest_result = (largest_result > larger) ? largest_result : larger;
                 }
             }
         }
@@ -147,29 +160,18 @@ void build_from_masks(int ma, int mb, int mc) {
 
 int main() {
     int mask_a, mask_b, mask_c;
-    mask_a = 0x7;
-    int dig_a[10];
-    const int ndig_a = mask_to_digits(mask_a, dig_a);
-    printf("ndig(%x): %d\n", mask_a, ndig_a);
-    int i;
-    for(i = 0; i < ndig_a; i++) {
-        printf("digit: %d\n", dig_a[i]);
-    }
-
-    int nperm = 0;
-    build_permutations(0, 0, 0, ndig_a, dig_a, &nperm, NULL);
-
-    /*
-    for(mask_A = 1; mask_A < (1 << 10); mask_A++) {
-        for(mask_B = 1; mask_B < (1 << 10); mask_B++) {
-            if(mask_A & mask_B) {
+    for(mask_a = 1; mask_a < (1 << 10); mask_a++) {
+        for(mask_b = 1; mask_b < (1 << 10); mask_b++) {
+            if(mask_a & mask_b) {
                 continue;
             }
 
-            mask_C = ~(mask_A | mask_B) & ((1 << 10) - 1);
+            mask_c = ~(mask_a | mask_b) & ((1 << 10) - 1);
 
-            build_from_masks(mask_A, mask_B, mask_C);
+            build_from_masks(mask_a, mask_b, mask_c);
         }
-    }*/
+    }
+
+    printf("largest concatenated product: %ld\n", largest_result);
     return 0;
 }
